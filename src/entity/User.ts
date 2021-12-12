57fdab1bb5428919015233c007c498488be28567
flaviojaf21 <flaviojaf@outlook.com>
2021-12-12 16:04:04 -0300

    First Commit

---
+++ src/entity/User.ts
@@ -0,0 +1,33 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    } from 'typeorm';
+
+@Entity ()
+    export class User{
+   
+    @PrimaryGeneratedColumn ('uuid')   
+    id:string;
+
+    @Column({nullable: true })
+    firstName:string;
+
+    @Column()
+    lastName:string;
+
+    @Column()
+    accountNumber:number;
+
+    @Column()
+    accountDigit:number;
+
+    @Column()
+    wallet:number;
+
+    @Column()
+    email:string;
+
+    @Column()
+    password:string;
+}
